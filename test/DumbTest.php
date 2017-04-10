319d441450f5f12762004b8d916da1c2833c62e8
Wanderson <wandersonwhcr@gmail.com>
2017-04-10 11:17:33 -0300

    Create a dumb test

---
+++ test/DumbTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ZendTest\Romans;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Dumb Test
+ */
+class DumbTest extends TestCase
+{
+    /**
+     * Test True
+     */
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
