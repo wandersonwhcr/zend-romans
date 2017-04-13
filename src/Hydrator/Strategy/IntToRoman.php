9dfc27e09ea847d0dbf3d88f70372f9f12db124e
Wanderson <wandersonwhcr@gmail.com>
2017-04-13 16:20:24 -0300

    Create hydrate and extract strategies

--- src/Hydrator/Strategy/IntToRoman.php
+++ src/Hydrator/Strategy/IntToRoman.php
@@ -3,6 +3,8 @@
 namespace Zend\Romans\Hydrator\Strategy;
 
 use Zend\Hydrator\Strategy\StrategyInterface;
+use Zend\Romans\Filter\IntToRoman as IntToRomanFilter;
+use Zend\Romans\Filter\RomanToInt as RomanToIntFilter;
 
 /**
  * Integer to Roman Number Hydrator Strategy
@@ -10,10 +12,77 @@ use Zend\Hydrator\Strategy\StrategyInterface;
 class IntToRoman implements StrategyInterface
 {
     /**
+     * IntToRoman Filter
+     * @type IntToRomanFilter
+     */
+    private $intToRomanFilter;
+
+    /**
+     * RomanToInt Filter
+     * @type RomanToIntFilter
+     */
+    private $romanToIntFilter;
+
+    /**
+     * Default Constructor
+     */
+    public function __construct()
+    {
+        $this
+            ->setIntToRomanFilter(new IntToRomanFilter())
+            ->setRomanToIntFilter(new RomanToIntFilter());
+    }
+
+    /**
+     * Set IntToRoman Filter
+     *
+     * @param  IntToRomanFilter $intToRomanFilter IntToRoman Filter Object
+     * @return self             Fluent Interface
+     */
+    public function setIntToRomanFilter(IntToRomanFilter $intToRomanFilter) : self
+    {
+        $this->intToRomanFilter = $intToRomanFilter;
+        return $this;
+    }
+
+    /**
+     * Get IntToRoman Filter
+     *
+     * @return IntToRomanFilter IntToRoman Filter Object
+     */
+    public function getIntToRomanFilter() : IntToRomanFilter
+    {
+        return $this->intToRomanFilter;
+    }
+
+    /**
+     * Set RomanToInt Filter
+     *
+     * @param  RomanToIntFilter $romanToIntFilter RomanToInt Filter Object
+     * @return self             Fluent Interface
+     */
+    public function setRomanToIntFilter(RomanToIntFilter $romanToIntFilter) : self
+    {
+        $this->romanToIntFilter = $romanToIntFilter;
+        return $this;
+    }
+
+    /**
+     * Get RomanToInt Filter
+     *
+     * @return RomanToIntFilter RomanToInt Filter Object
+     */
+    public function getRomanToIntFilter() : RomanToIntFilter
+    {
+        return $this->romanToIntFilter;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function extract($value)
     {
+        return $this->getRomanToIntFilter()->filter($value);
     }
 
     /**
@@ -21,5 +90,6 @@ class IntToRoman implements StrategyInterface
      */
     public function hydrate($value)
     {
+        return $this->getIntToRomanFilter()->filter($value);
     }
 }
--- test/Hydrator/Strategy/IntToRomanTest.php
+++ test/Hydrator/Strategy/IntToRomanTest.php
@@ -26,4 +26,24 @@ class IntToRomanTest extends TestCase
     {
         $this->assertInstanceOf(StrategyInterface::class, $this->strategy);
     }
+
+    /**
+     * Test Extract
+     */
+    public function testExtract()
+    {
+        $this->assertSame(1, $this->strategy->extract('I'));
+        $this->assertSame(5, $this->strategy->extract('V'));
+        $this->assertSame(10, $this->strategy->extract('X'));
+    }
+
+    /**
+     * Test Hydrate
+     */
+    public function testHydrate()
+    {
+        $this->assertSame('I', $this->strategy->hydrate(1));
+        $this->assertSame('V', $this->strategy->hydrate(5));
+        $this->assertSame('X', $this->strategy->hydrate(10));
+    }
 }
