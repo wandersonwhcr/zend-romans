b3130b65fb816d8992ec5a35734570726b33d747
Wanderson <wandersonwhcr@gmail.com>
2017-04-19 13:00:27 -0300

    Create Lexer service with a service factory

---
+++ src/Lexer/Factory/Lexer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Zend\Romans\Lexer\Factory;
+
+use Interop\Container\ContainerInterface;
+use Romans\Grammar\Grammar as RomansGrammar;
+use Romans\Lexer\Lexer as RomansLexer;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+/**
+ * Lexer Factory
+ */
+class Lexer implements FactoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $config = null)
+    {
+        return new RomansLexer(
+            $container->get(RomansGrammar::class)
+        );
+    }
+}
