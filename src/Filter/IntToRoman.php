a0fbb1fe482744c68dca47505b81fdd1f76f8efe
Wanderson <wandersonwhcr@gmail.com>
2017-04-19 13:33:57 -0300

    Remove direct dependency into constructor

--- src/Filter/IntToRoman.php
+++ src/Filter/IntToRoman.php
@@ -3,7 +3,7 @@
 namespace Zend\Romans\Filter;
 
 use Romans\Filter\Exception as FilterException;
-use Romans\Filter\IntToRoman as BaseIntToRoman;
+use Romans\Filter\IntToRoman as IntToRomanFilter;
 use Zend\Filter\FilterInterface;
 
 /**
@@ -12,6 +12,48 @@ use Zend\Filter\FilterInterface;
 class IntToRoman implements FilterInterface
 {
     /**
+     * Base Integer to Roman Number Filter
+     * @type IntToRomanFilter
+     */
+    private $intToRomanFilter;
+
+    /**
+     * Default Constructor
+     *
+     * @param IntToRomanFilter $intToRomanFilter IntToRomanFilter Object
+     */
+    public function __construct(IntToRomanFilter $intToRomanFilter = null)
+    {
+        if (! isset($intToRomanFilter)) {
+            $intToRomanFilter = new IntToRomanFilter();
+        }
+
+        $this->setIntToRomanFilter($intToRomanFilter);
+    }
+
+    /**
+     * Set Integer to Roman Number Filter
+     *
+     * @param  IntToRomanFilter $intToRomanFilter IntToRomanFilter Object
+     * @return self             Fluent Interface
+     */
+    public function setIntToRomanFilter(IntToRomanFilter $intToRomanFilter) : self
+    {
+        $this->intToRomanFilter = $intToRomanFilter;
+        return $this;
+    }
+
+    /**
+     * Get Integer to Roman Number Filter
+     *
+     * @return IntToRomanFilter IntToRomanFilter Object
+     */
+    public function getIntToRomanFilter() : IntToRomanFilter
+    {
+        return $this->intToRomanFilter;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function filter($value)
@@ -19,7 +61,7 @@ class IntToRoman implements FilterInterface
         $result = null;
 
         try {
-            $result = (new BaseIntToRoman())->filter($value);
+            $result = $this->getIntToRomanFilter()->filter($value);
         } catch (FilterException $e) {
             // default value: null
         }
